49d6f39988f35fa1320d3274a4d36078393c026c
smuralee <suraj@surajmuraleedharan.com>
2021-03-14 20:37:24 +0530

    WIP: Added buildspec and appspec

--- orders-api/src/test/java/com/smuralee/ProductOrdersApiApplicationTest.java
+++ orders-api/src/test/java/com/smuralee/ProductOrdersApiApplicationTest.java
@@ -1,24 +1,36 @@
 package com.smuralee;
 
 import com.smuralee.config.AppConfig;
+import com.smuralee.config.model.RDSSecret;
 import com.smuralee.service.ProductOrderController;
+import com.smuralee.util.SecretsClient;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.Spy;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 class ProductOrdersApiApplicationTest {
 
-    @Autowired
+    @Spy
+    private static final AppConfig appConfig = new AppConfig();
+    @Spy
+    private static final SecretsClient secretsClient = new SecretsClient(appConfig);
+    @MockBean
     private ProductOrderController controller;
 
-    @Autowired
-    private AppConfig appConfig;
+    @BeforeAll
+    static void setUp() {
+        RDSSecret rdsSecret = new RDSSecret();
+        when(secretsClient.getSecret()).thenReturn(rdsSecret);
+    }
 
     @Test
     void contextLoads() {
