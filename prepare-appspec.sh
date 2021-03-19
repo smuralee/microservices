9c42c3b8535ad6961818c42e846a3dfde064ef5e
smuralee <suraj@surajmuraleedharan.com>
2021-03-19 14:09:33 +0530

    WIP: X-Ray and AppSpec updates

--- prepare-appspec.sh
+++ prepare-appspec.sh
@@ -5,8 +5,8 @@ for service_name in "${service_names[@]}"
 do
   aws --version
   VERSION=$(aws ecs list-task-definitions --family-prefix $service_name --query 'taskDefinitionArns[-1]' --output text | sed "s/.*://")
-  sed -i 's@ACCOUNT_ID@'$ACCOUNT_ID'@g' appspec.yml
-  sed -i 's@AWS_REGION@'$AWS_REGION'@g' appspec.yml
-  sed -i 's@${service_name}_version@'$VERSION'@g' appspec.yml
+  sed -i 's@ACCOUNT_ID@'$ACCOUNT_ID'@g' appspec.yaml
+  sed -i 's@AWS_REGION@'$AWS_REGION'@g' appspec.yaml
+  sed -i 's@orders_version@'$VERSION'@g' appspec.yaml
   echo Completed appspec.yml update on `date`
 done
